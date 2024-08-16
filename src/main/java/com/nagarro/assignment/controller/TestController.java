5752110193f42e8b72c0be8fc01a7d6ea3b42a6f
ramsingh02 <ram.singh01@nagarro.com>
2024-08-16 15:18:48 +0530

    first

---
+++ src/main/java/com/nagarro/assignment/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.nagarro.assignment.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test/api")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test(){
+        return new ResponseEntity<>("Test Controller", HttpStatus.OK);
+    }
+}
